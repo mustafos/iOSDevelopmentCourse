e584a0165c57ae4318a13bd7ae5f574d8810ed30
Mustafa Bekirov <mustafos1999@gmail.com>
2023-01-01 20:29:14 +0100

    refactore: create generic onboarding view controller
    This commit adds a generic onboarding view controller that can be reused across
    multiple onboarding screens.

--- BankApp/BankApp/AppDelegate.swift
+++ BankApp/BankApp/AppDelegate.swift
@@ -18,8 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
 //        window?.rootViewController = LoginViewController()
-//        window?.rootViewController = OnboardingContainerViewController()
-        window?.rootViewController = OnboardingViewController()
+        window?.rootViewController = OnboardingContainerViewController()
+//        window?.rootViewController = OnboardingViewController(heroImageName: "delorean", titleText: "BankApp is faster and more reliable banking in your pocket. We present another way of looking at things.")
         
         return true
     }

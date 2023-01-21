c8a928f26a643a4e531b22791232bf4503687dfd
Mustafa Bekirov <mustafos1999@icloud.com>
2023-01-21 22:23:19 +0100

    test: Add unit tests for CurrencyFormatter

---
+++ BankApp/BankAppUnitTests/CurrencyFormatterTests.swift
@@ -0,0 +1,47 @@
+//
+//  CurrencyFormatterTests.swift
+//  BankAppUnitTests
+//
+//  Created by Mustafa Bekirov on 21.01.2023.
+//
+
+import Foundation
+import XCTest
+
+@testable import BankApp
+
+class Test: XCTestCase {
+    var formatter: CurrencyFormatter!
+    
+    override func setUp() {
+        super.setUp()
+        formatter = CurrencyFormatter()
+    }
+    
+    func testShouldBeVisible() throws {
+        let result = formatter.breakIntoDollarsAndCents(929466.23)
+        XCTAssertEqual(result.0, "929,466")
+        XCTAssertEqual(result.1, "23")
+    }
+    
+    // Challenge: You write
+    func testDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(929466.23)
+        XCTAssertEqual(result, "$929,466.23")
+    }
+    
+    // Challenge: You write
+    func testZeroDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(0.00)
+        XCTAssertEqual(result, "$0.00")
+    }
+    
+    func testDollarsFormattedWithCurrencySymbol() throws {
+        let locale = Locale.current
+        let currencySymbol = locale.currencySymbol!
+        
+        let result = formatter.dollarsFormatted(929466.23)
+        print("\(currencySymbol)")
+        XCTAssertEqual(result, "\(currencySymbol)929,466.23")
+    }
+}

d8d2c15d096d8bd3fa1e0f29b0dce8f775b33898
Mustafa Bekirov <mustafos1999@icloud.com>
2023-01-24 20:16:24 +0100

    feat: Shake signin button if login fails

--- BankApp/BankApp/LoginViewController.swift
+++ BankApp/BankApp/LoginViewController.swift
@@ -153,12 +153,13 @@ extension LoginViewController {
             return
         }
         
-//        if username.isEmpty || password.isEmpty {
-//            configureView(withMessage: "Username / password cannot be blank")
-//            return
-//        }
+        // Check for blanks
+        if username.isEmpty || password.isEmpty {
+            configureView(withMessage: "Username / password cannot be blank")
+            return
+        }
         
-        if username == "" && password == "" {
+        if username == "Mustafa" && password == "Bekirov" {
             signInButton.configuration?.showsActivityIndicator = true
             delegate?.didLogin()
         } else {
@@ -166,9 +167,21 @@ extension LoginViewController {
         }
     }
     
-    func configureView(withMessage message: String) {
+    private func configureView(withMessage message: String) {
         errorMessageLabel.isHidden = false
         errorMessageLabel.text = message
+        shakeButton()
+    }
+        
+    private func shakeButton() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+
+        animation.isAdditive = true
+        signInButton.layer.add(animation, forKey: "shake")
     }
 }
 

fbfaa206e494b890d1759971a685a0a6ec72ea59
Mustafa Bekirov <mustafos1999@icloud.com>
2023-02-24 22:35:22 +0100

    feat: create new password project

---
+++ Password/Password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Mustafa Bekirov on 24.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
